e9db29be2f54567c00bc7cbfad708926162878bb
jheilhet <shirley.mramirez@yahoo.com>
2018-02-01 23:10:18 -0700

    added comments in html files and post routes code in apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,20 @@
 var friendData = require('../data/friends.js');
 
 module.exports = function(app) {
-    app.get('/api/friend', function(req, res) {
+
+    // a GET route with the url /api/friends will display a JSON of all possible friends
+    app.get('/api/friends', function(req, res) {
         res.json(friendData);
     });
+
+    // A POST routes this will be used to handle incoming survey results.
+    app.post('/api/friends', function(req, res) {
+
+        var newFriendData = req.body;
+
+        friendData.push(newFriendData);
+
+        res.json(newFriendData);
+    });
+
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,12 @@ var path = require('path');
 
 module.exports = function(app) {
 
+    // a get route that will display the survey page
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 
+    // A default, catch-all route leads to home page
     app.use("/", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
