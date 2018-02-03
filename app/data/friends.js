59e154020e53090e889fbcd452a926b506c3a5d5
jheilhet <shirley.mramirez@yahoo.com>
2018-02-02 18:18:16 -0700

    modified survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,11 @@
 module.exports = [{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-}];
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Jasmine",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1, 3, 4, 2, 5, 1, 2, 3, 4, 5]
+    }
+];
