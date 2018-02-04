193e21788e9ddff87928a1882bed109bdd0cb089
jheilhet <shirley.mramirez@yahoo.com>
2018-02-03 19:32:31 -0700

    editted friends.js and added code for button click validation

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,5 +7,45 @@ module.exports = [{
         name: "Jasmine",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: [1, 3, 4, 2, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        name: "Rudy",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5, 4, 1, 2, 3, 4, 5, 4, 3, 2]
+    },
+    {
+        name: "Albert",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [2, 3, 4, 5, 5, 4, 3, 2, 1, 5]
+    },
+    {
+        name: "Laine",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1, 2, 4, 4, 5, 3, 2, 1, 4, 2]
+    },
+    {
+        name: "Carmen",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1, 3, 4, 2, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        name: "Eddie",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [2, 3, 5, 2, 2, 1, 3, 3, 3, 4]
+    },
+    {
+        name: "Jasmine",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1, 2, 4, 4, 5, 2, 2, 5, 4, 2]
+    },
+    {
+        name: "Josephine",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1, 2, 5, 2, 1, 1, 4, 3, 1, 1]
+    },
+    {
+        name: "Victor",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5, 3, 2, 2, 1, 1, 3, 3, 2, 5]
     }
 ];
